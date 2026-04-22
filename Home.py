ccef1cbb889501d5274454a7569e9d3952ca04a7
Somya jain <gungunjain049@gmail.com>
2026-04-23 01:03:00 +0530

    Delete Home.py

--- Home.py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="PlacementIQ Ultimate", layout="wide")
-
-st.title("PlacementIQ Ultimate System")
-st.write("Includes prediction, analytics, faculty tools, search, export, and role-based intelligence.")
